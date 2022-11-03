caf11564df5a930c74d851b2bbbadc166556f285
A1 <nai39sk@ya.ru>
2022-11-03 16:39:43 +0400

    fix popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,18 @@
 
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button');
-let editButton = document.querySelector('.profile-info__edit-button');
-
-closeButton.addEventListener('click', popupToggle);
-editButton.addEventListener('click', popupToggle);
-
-function popupToggle() {
-  popup.classList.toggle('popup_opened');
-}
-
-let profileName = document.querySelector('.profile-info__name');
-let profileTitle = document.querySelector('.profile-info__title');
+let editButton = document.querySelector('.profile__edit-button');
+let profileName = document.querySelector('.profile__name');
+let profileTitle = document.querySelector('.profile__title');
 let inputName = document.querySelector('#popup__profile-name');
 let inputTitle = document.querySelector('#popup__profile-title');
 let formElement = document.querySelector('.popup__container');
 
-inputName.value = profileName.textContent;
-inputTitle.value = profileTitle.textContent;
+function popupToggle() {
+  inputName.value = profileName.textContent;
+  inputTitle.value = profileTitle.textContent;
+  popup.classList.toggle('popup_opened');
+}
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -26,4 +21,6 @@ function formSubmitHandler (evt) {
   popupToggle();
 }
 
-formElement.addEventListener('submit', formSubmitHandler); 
+closeButton.addEventListener('click', popupToggle);
+editButton.addEventListener('click', popupToggle);
+formElement.addEventListener('submit', formSubmitHandler);
